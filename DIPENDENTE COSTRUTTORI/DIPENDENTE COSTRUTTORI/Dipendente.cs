ec70c6de32729d97fca017323ef801af8cc05f8d
Avenez <145536778+Avenez@users.noreply.github.com>
2024-01-30 14:13:19 +0100

    Add files via upload

---
+++ DIPENDENTE COSTRUTTORI/DIPENDENTE COSTRUTTORI/Dipendente.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlTypes;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DIPENDENTE_COSTRUTTORI
+{
+    internal class Dipendente
+    {
+        private string nome;
+        private string cognome;
+        private int eta;
+        private string dipartimento;
+
+        public Dipendente() { }
+
+        public Dipendente(string nome, string cognome, int eta)
+        {
+            this.nome = nome;
+            this.cognome = cognome;
+            this.eta = eta;
+        }
+
+        public Dipendente(string nome, string cognome, int eta, string dipartimento = "Logistica")
+        {
+            this.nome = nome;
+            this.cognome = cognome;
+            this.eta = eta;
+            this.dipartimento = dipartimento;
+        }
+
+        public string Nome
+        {
+            get { return nome; }
+            set { nome = value; }
+        }
+
+        public string Cognome
+        {
+            get { return cognome; }
+            set { cognome = value; }
+        }
+
+        public int Eta
+        {
+            get { return eta; }
+            set { eta = value; }
+        }
+
+        public string Dipartimento
+        {
+            get { return dipartimento; }
+            set { dipartimento = value; }
+        }
+
+        public string GetDettagli()
+        {
+            return $"{nome} {cognome} {eta} {dipartimento}";
+        }
+
+
+        public static void defineTriangle(int lato1, int lato2 , int lato3) {
+
+            if ((lato1 == lato2) && (lato1 == lato3) && (lato3 == lato2))
+            { Console.WriteLine("Il triangolo è Equilatero"); }
+
+            else if ((lato1 != 0 && lato2 == lato3) || (lato2 != 0 && lato1 == lato3) || (lato3 != 0 && lato2 == lato1) || (lato1 == lato2) || (lato1 == lato3) || (lato3 == lato2)) {
+
+                Console.WriteLine("Il triangolo è Isoscele");
+
+            } else if ((lato1 != lato2) && (lato1 != lato3) && (lato2 != lato3)) {
+
+                Console.WriteLine("Il triangolo è Scaleno");
+
+            } else {
+
+                Console.WriteLine("Non hai fornito i lati di un trinagolo");
+
+            }
+        
+        
+        
+        }
+
+    }
+}
---
+++ DIPENDENTE COSTRUTTORI/DIPENDENTE COSTRUTTORI/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DIPENDENTE_COSTRUTTORI
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Dipendente dipendente1 = new Dipendente("Marco", "Perri", 30);
+            Dipendente dipendente2 = new Dipendente("Giorgio", "Mastrotta", 44, "Amministrazione");
+            Dipendente dipendente3 = new Dipendente("Aldo", "Baglio", 35);
+
+            Console.WriteLine("\n");
+
+            Console.WriteLine(dipendente1.Nome);
+            Console.WriteLine(dipendente1.Cognome);
+            Console.WriteLine(dipendente1.Eta);
+            Console.WriteLine(dipendente1.Dipartimento);
+            Console.WriteLine(dipendente1.GetDettagli());
+
+
+
+
+            Console.WriteLine("\n");
+
+
+            Console.WriteLine(dipendente2.Nome);
+            Console.WriteLine(dipendente2.Cognome);
+            Console.WriteLine(dipendente2.Eta);
+            Console.WriteLine(dipendente2.Dipartimento);
+            Console.WriteLine(dipendente2.GetDettagli());
+
+            dipendente2.Dipartimento = "Distruzione";
+            Console.WriteLine(dipendente2.GetDettagli());
+
+            Console.WriteLine("\n");
+
+
+            Console.WriteLine(dipendente3.Nome);
+            Console.WriteLine(dipendente3.Cognome);
+            Console.WriteLine(dipendente3.Eta);
+            Console.WriteLine(dipendente3.Dipartimento);
+            Console.WriteLine(dipendente3.GetDettagli());
+
+            Console.WriteLine("\n");
+
+            dipendente3.Dipartimento = "Vendite";
+            Console.WriteLine(dipendente3.Dipartimento);
+            Console.WriteLine(dipendente3.GetDettagli());
+
+
+            Console.WriteLine("Indica la lunghezza del primo lato !");
+            int lato1 = Convert.ToInt16(Console.ReadLine());
+
+            Console.WriteLine("Indica la lunghezza del secondo lato !");
+            int lato2 = Convert.ToInt16(Console.ReadLine());
+
+            Console.WriteLine("Indica la lunghezza del terzo lato !");
+            int lato3 = Convert.ToInt16( Console.ReadLine());
+
+            Dipendente.defineTriangle(lato1, lato2, lato3);
+
+
+
+
+
+            Console.ReadLine();
+        }
+    }
+}
